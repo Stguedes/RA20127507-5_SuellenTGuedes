c123745dbb472a309a3b99068e207887c0e4b51a
Suellen Teixeira <suellen-tg@hotmail.com>
2021-08-27 12:20:15 -0300

    modificado o local do css do detalhes

--- pages/detalhes.php
+++ pages/detalhes.php
@@ -18,6 +18,7 @@ $produto = selecionarProduto($id);
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/css/basico.css" />
+    <link rel="stylesheet" href="/css/detalhes.css">
     <title>detalhes</title>
 </head>
 
